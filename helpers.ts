0965bc6dcb1fdc588c6c4bae179c385acb5c2e1c
Antonnn555 <137509448+Antonnn555@users.noreply.github.com>
2025-11-12 16:06:40 +0200

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T14:06:37.569Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-12T14:06:39.465Z
+Commit: Patch security vulnerability in authentication
